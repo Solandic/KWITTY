121b8bd0d92fb52d6f523ebc410fbe6cc1a7bbd4
Yaroslav Musiychuk <65865743+Yarik353@users.noreply.github.com>
2024-11-22 08:44:04 +0300

    1

--- framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
+++ framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
@@ -1,2 +1,2 @@
-function t(a,e){return{description:"just a lil KWAT doing KWAT things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$KWAT"}}export{t as a};
+function t(a,e){return{description:"just a lil MWEOW doing MWEOW things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$MWEOW"}}export{t as a};
 //# sourceMappingURL=chunk-WCA4E3HV.mjs.map
