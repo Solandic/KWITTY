05a221a750bf8d23ed342df79cbd15c6089f6232
Yaroslav Musiychuk <65865743+Yarik353@users.noreply.github.com>
2024-12-03 10:00:28 +0300

    1

--- framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
+++ framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
@@ -1,2 +1,2 @@
-function t(a,e){return{description:"just a lil KWATY doing KWATY things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$KWATY"}}export{t as a};
+function t(a,e){return{description:"just a lil PWURRY doing PWURRY things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$PWURRY"}}export{t as a};
 //# sourceMappingURL=chunk-WCA4E3HV.mjs.map
