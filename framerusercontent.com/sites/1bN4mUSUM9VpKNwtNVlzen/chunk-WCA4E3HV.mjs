61e53ee11f4136f185a568c636e4dab9f59aaa4d
Yaroslav Musiychuk <65865743+Yarik353@users.noreply.github.com>
2024-12-21 09:50:39 +0300

    1

--- framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
+++ framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
@@ -1,2 +1,2 @@
-function t(a,e){return{description:"just a lil CWATIE doing CWATIE things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$CWATIE"}}export{t as a};
+function t(a,e){return{description:"just a lil CWAT doing CWAT things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$CWAT"}}export{t as a};
 //# sourceMappingURL=chunk-WCA4E3HV.mjs.map
