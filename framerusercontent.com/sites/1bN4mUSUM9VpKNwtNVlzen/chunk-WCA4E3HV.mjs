319efe52764f5dbeab7155461f0c6519ebf9ed70
Yaroslav Musiychuk <65865743+Yarik353@users.noreply.github.com>
2024-12-28 10:57:19 +0300

    1

--- framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
+++ framerusercontent.com/sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
@@ -1,2 +1,2 @@
-function t(a,e){return{description:"just a lil FWELIN doing FWELIN things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$FWELIN"}}export{t as a};
+function t(a,e){return{description:"just a lil CWATTY doing CWATTY things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$CWATTY"}}export{t as a};
 //# sourceMappingURL=chunk-WCA4E3HV.mjs.map
